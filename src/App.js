ada12d5a60cf3b59cd4632732a4a722a683d0663
Tom Booth <tombeezle@gmail.com>
2023-01-19 12:48:06 -0800

    header/main/footer rendering

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Header from './components/Header/Header.js';
+import Main from './components/Main/Main.js';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
